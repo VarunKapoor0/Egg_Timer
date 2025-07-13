6a00c1af20d1c10050982fa11128db33fac7c6f6
VarunKapoor0 <varunkap@usc.edu>
2025-07-13 15:18:20 -0700

    Update EggTypeRepository.kt
    Fixed the time of soft boiled eggs.

--- app/src/main/java/com/varun/egg_timer/data/EggTypeRepository.kt
+++ app/src/main/java/com/varun/egg_timer/data/EggTypeRepository.kt
@@ -5,7 +5,7 @@ import com.varun.egg_timer.models.EggType
 class EggTypeRepository {
 
     fun getEggTypes(): List<EggType> = listOf(
-        EggType("Soft-Boiled", 1, "Runny yolk, set whites"),
+        EggType("Soft-Boiled", 5, "Runny yolk, set whites"),
         EggType("Medium-Boiled", 7, "Slightly soft yolk"),
         EggType("Hard-Boiled", 10, "Fully cooked yolk and whites")
     )
